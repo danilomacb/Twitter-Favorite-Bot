bc6f2969e8efca806e5ac7cdf60ffc859e9a99db
danilomacb <danilo.bakun@gmail.com>
2021-09-22 07:46:22 -0300

    using env vars, removing comments, changing params, unnecessary loop removed, new check for errors on post and new log on app.js

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const Twitter = require('twitter');
-const config = require('./config.js');
-const T = new Twitter(config);
+require("dotenv").config();
 
-// Set up your search parameters
+const Twitter = require("twitter");
+const T = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
 const params = {
-  q: '#nodejs',
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-}
+  q: "#gamedev",
+  count: process.env.COUNT,
+  result_type: "recent",
+  lang: "en",
+};
 
-// Initiate your search using the above paramaters
-T.get('search/tweets', params, (err, data, response) => {
-  // If there is no error, proceed
-  if(err){
+T.get("search/tweets", params, (err, data, response) => {
+  if (err) {
     return console.log(err);
   }
 
-  // Loop through the returned tweets
-  const tweetsId = data.statuses
-    .map(tweet => ({ id: tweet.id_str }));
-
-  tweetsId.map(tweetId => {
-    T.post('favorites/create', tweetId, (err, response) => {
-      if(err){
-        return console.log(err[0].message);
-      }
-
-      const username = response.user.screen_name;
-      const favoritedTweetId = response.id_str;
-      console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
-
+  data.statuses.map((tweet) => {
+    T.post("favorites/create", { id: tweet.id_str }, (err, response) => {
+      if (err && err[0] && err[0].message && err[0].code !== 139) return console.log(err[0].message);
+      if (err && err.message) return console.log(err.message);
     });
   });
-})
+});
+
+const today = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+console.log(`RUNNING ON ${today}`);
